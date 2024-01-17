63ab999b1181534ac8b59a8f85f8249ce10d26b3
sakshideveops <149102755+sakshideveops@users.noreply.github.com>
2024-01-17 14:41:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-<<<<<<< HEAD
  res.send('hello world');
-=======
- res.send('hello world');
->>>>>>> 84ed444d5da2ebdcc193b27b4e1d0baa3946eb6c
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
